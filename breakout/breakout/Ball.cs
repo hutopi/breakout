b1ae400b96f89d2d1a67a020fc2e5adae932814f
asukero <thomas.fossati59@gmail.com>
2015-05-25 14:50:53 +0200

    Fixed bouncing when the ball hits the edge of the bat

--- breakout/breakout/Ball.cs
+++ breakout/breakout/Ball.cs
@@ -18,7 +18,7 @@ namespace breakout {
             //the Postions are taken from the top left of the circle and not the center so you have to to move the postion with half the texture of the sprite to a have a circle hitbox that fits correclty the circle
             get { return new Circle((int)Position.X + Texture.Width / 2, (int)Position.Y + Texture.Height / 2, (double)Texture.Width / 2); }
         }
-       
+
 
         public Ball(int screenWidth, int screenHeight) {
             this.screenHeight = screenHeight;
@@ -32,7 +32,7 @@ namespace breakout {
 
             Direction = new Vector2(0, -1);
 
-            Speed = 0.2f;
+            Speed = 0.3f;
 
         }
 
@@ -63,32 +63,44 @@ namespace breakout {
             // du coup si vous tapez pile au milieu, la balle repart verticalement, ensuite plus vous tapez en vous éloignant du milieu plus la composante horizontale X du vecteur de la direction sera importante
             // c'est pas parfait mais ça permet de faire plus de mouvements que de simplement inverser la direction Y et donc de ne pas subir les mouvements de la balles dues au rebons sur les autres composants du jeu.
             if ((Direction.Y > 0 && this.hitbox.IntersectsRec(batHitBox))) {
-                if (this.hitbox.X <= batHitBox.X+batHitBox.Width && this.hitbox.X > batHitBox.X+batHitBox.Width/2) {
+                if (this.hitbox.X <= batHitBox.X + batHitBox.Width && this.hitbox.X > batHitBox.X + batHitBox.Width / 2) {
 
                     double gapBetweenBatAndBall = batHitBox.X + batHitBox.Width - this.hitbox.X; //détermine l'écart X de la balle par rapport à l'extrémité droite de la raquette
                     double theta = (gapBetweenBatAndBall * (Math.PI / 2)) / (batHitBox.Width / 2); // produit en croix pour une valeur théta en radian de l'angle que l'on souhaite obtenir en fonction de l'écart gapBetweenBatAndBall
-                    Direction = new Vector2((float)Math.Cos(theta), -(float)Math.Sin(theta));
+                    Console.WriteLine(theta);
+                    if (theta <= 0.1) {
+                        Direction = new Vector2((float)Math.Cos(theta), -0.1f);
+                        
+                    } else {
+                        Direction = new Vector2((float)Math.Cos(theta), -(float)Math.Sin(theta));
+                    }
 
-                    
+
                     Console.WriteLine(Direction);
                 }
-                if (this.hitbox.X < batHitBox.X + batHitBox.Width/2 && this.hitbox.X > batHitBox.X) {
+                if (this.hitbox.X < batHitBox.X + batHitBox.Width / 2 && this.hitbox.X >= batHitBox.X) {
 
                     double gapBetweenBatAndBall = this.hitbox.X - batHitBox.X;
                     double theta = (gapBetweenBatAndBall * (Math.PI / 2)) / (batHitBox.Width / 2);
-                    Direction = new Vector2(-(float)Math.Cos(theta), -(float)Math.Sin(theta));
+                    if (theta <= 0.1) {
+                        Direction = new Vector2(-(float)Math.Cos(theta), -0.1f);
+                        Console.WriteLine("perdu");
+                    } else {
+                        Direction = new Vector2(-(float)Math.Cos(theta), -(float)Math.Sin(theta));
+                    }
+                    
 
-                   
+
                     Console.WriteLine(Direction);
                 }
-                if (this.hitbox.X == batHitBox.X+batHitBox.Width/2) {
+                if (this.hitbox.X == batHitBox.X + batHitBox.Width / 2) {
                     Direction = new Vector2(0, -1);
                     Console.WriteLine(Direction);
                 }
                 //Do we speed up the ball each time it hits the bat ?
                 //Speed += 0.05f;
 
-                
+
 
             }
 
