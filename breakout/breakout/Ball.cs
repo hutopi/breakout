a29b0a25c2d3023ab2adb52c69c0c01bec2aa0e3
asukero <thomas.fossati59@gmail.com>
2015-05-25 19:25:05 +0200

    Added bouncing behavior on the bat

--- breakout/breakout/Ball.cs
+++ breakout/breakout/Ball.cs
@@ -46,6 +46,7 @@ namespace breakout {
 
         public void Update(GameTime gameTime, Rectangle batHitBox) {
 
+            //bouncing on the walls
             if ((Position.Y <= 0 && Direction.Y < 0)) {
 
                 Direction = new Vector2(Direction.X, -Direction.Y);
@@ -55,21 +56,39 @@ namespace breakout {
                 Direction = new Vector2(-Direction.X, Direction.Y);
             }
 
-
+            //bouncing on the bat
+            //Pour cette part je commente en FR pour le moment parce que c'est compliqué
+            // grosso le merdo le but est de faire rebondir la balle de manière différente pour chaque coté de la raquette (gauche, droite ou au mileu)
+            // puis grace aux cours de trigo, je donne une direction à la balle plus moins verticale en fonction de son rapprochement avec le centre de la raquette
+            // du coup si vous tapez pile au milieu, la balle repart verticalement, ensuite plus vous tapez en vous éloignant du milieu plus la composante horizontale X du vecteur de la direction sera importante
+            // c'est pas parfait mais ça permet de faire plus de mouvements que de simplement inverser la direction Y et donc de ne pas subir les mouvements de la balles dues au rebons sur les autres composants du jeu.
             if ((Direction.Y > 0 && this.hitbox.IntersectsRec(batHitBox))) {
-                if (this.Position.X > batHitBox.X) {
-                    Direction = new Vector2(-1, -Direction.Y);
-                    //Console.WriteLine(Direction);
-                }
-                if (this.Position.X < batHitBox.X) {
-                    Direction = (new Vector2((batHitBox.X - this.Position.X) * Direction.X, -Direction.Y));
+                if (this.hitbox.X <= batHitBox.X+batHitBox.Width && this.hitbox.X > batHitBox.X+batHitBox.Width/2) {
+
+                    double gapBetweenBatAndBall = batHitBox.X + batHitBox.Width - this.hitbox.X; //détermine l'écart X de la balle par rapport à l'extrémité droite de la raquette
+                    double theta = (gapBetweenBatAndBall * (Math.PI / 2)) / (batHitBox.Width / 2); // produit en croix pour une valeur théta en radian de l'angle que l'on souhaite obtenir en fonction de l'écart gapBetweenBatAndBall
+                    Direction = new Vector2((float)Math.Cos(theta), -(float)Math.Sin(theta));
+
+                    
                     Console.WriteLine(Direction);
                 }
-                if (this.Position.X == batHitBox.X) {
-                    Direction = new Vector2(Direction.X, -Direction.Y);
+                if (this.hitbox.X < batHitBox.X + batHitBox.Width/2 && this.hitbox.X > batHitBox.X) {
+
+                    double gapBetweenBatAndBall = this.hitbox.X - batHitBox.X;
+                    double theta = (gapBetweenBatAndBall * (Math.PI / 2)) / (batHitBox.Width / 2);
+                    Direction = new Vector2(-(float)Math.Cos(theta), -(float)Math.Sin(theta));
+
+                   
                     Console.WriteLine(Direction);
                 }
-                Speed += 0.05f;
+                if (this.hitbox.X == batHitBox.X+batHitBox.Width/2) {
+                    Direction = new Vector2(0, -1);
+                    Console.WriteLine(Direction);
+                }
+                //Do we speed up the ball each time it hits the bat ?
+                //Speed += 0.05f;
+
+                
 
             }
 
