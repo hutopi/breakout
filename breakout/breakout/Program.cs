4b76a05e1e7fcc0a409381529db70e96488e418e
Hugo Caille <hugo.caille@isen-lille.fr>
2015-05-28 15:40:30 +0200

    XNA installation check on startup, alert dialogs on game exceptions.

--- breakout/breakout/Program.cs
+++ breakout/breakout/Program.cs
@@ -1,4 +1,7 @@
 using System;
+using System.IO;
+using System.Windows.Forms;
+using System.Reflection;
 
 namespace breakout
 {
@@ -10,10 +13,37 @@ namespace breakout
         /// </summary>
         static void Main(string[] args)
         {
-
             using (Breakout game = new Breakout())
             {
-                game.Run();
+                try
+                {
+                    Assembly.Load("Microsoft.Xna.Framework, Version=4.0.0.0, Culture=neutral, PublicKeyToken=842cf8be1de50553");
+                }
+                catch (FileNotFoundException ex)
+                {
+                    MessageBox.Show(
+                        "Unable to locate XNA Game Framework 4.0.",
+                        "XNA loading error",
+                        MessageBoxButtons.OK,
+                        MessageBoxIcon.Error
+                    );
+                    return;
+                }
+
+                try
+                {
+                    game.Run();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(
+                        String.Format("Unhandled exception: {0}", ex),
+                        "Game exception",
+                        MessageBoxButtons.OK,
+                        MessageBoxIcon.Error
+                    );
+                    game.Exit();
+                }
             }
         }
     }
